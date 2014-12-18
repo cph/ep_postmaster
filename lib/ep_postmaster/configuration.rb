c1ca14955ca984c8b99c2ce924b22382e814db21
Ben Govero <ben.govero@cph.org>
2014-12-18 14:08:58 -0600

    [bugfix] Refactored bounced_email_handler to accept and instantiate a string representation of a class (15m)

--- lib/ep_postmaster/configuration.rb
+++ lib/ep_postmaster/configuration.rb
@@ -1,14 +1,26 @@
 module EpPostmaster
   class Configuration
-    attr_accessor :mailgun_api_key, :mailer_sender, :bounced_email_handler
+    attr_accessor :mailgun_api_key, :mailer_sender
     
     def initialize
       @mailgun_api_key = ""
-      @bounced_email_handler = Class.new do
+      self.bounced_email_handler = Class.new do
         def self.handle_bounced_email(*)
           # noop
         end
       end
+    end
+    
+    def bounced_email_handler=(handler)
+      @__bounced_email_handler = handler
+    end
+    
+    # constantize passed strings the first time they're called, cache the result for subsequent calls
+    def bounced_email_handler
+      if @__bounced_email_handler.is_a?(String)
+        @__bounced_email_handler = @__bounced_email_handler.constantize
+      end
+      @__bounced_email_handler
     end
      
   end
