fd5f0ef88485572ba1f2333c76b320aa4577fd7d
Ben Govero and Bob Lail and Matthew Kobs <pair=ben.govero+bob.lail+matt.kobs@cphepdev.com>
2015-08-10 14:09:16 -0500

    [skip] Deleted unused line of code (1m)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,9 +10,6 @@ require "shoulda/context"
 require "turn"
 Rails.backtrace_cleaner.remove_silencers!
 
-# Load support files
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
-
 # Load fixtures from the engine
 if ActiveSupport::TestCase.method_defined?(:fixture_path=)
   ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
